67817e9e304af6e75d6f712769e86b95f7e15123
Flavien <livinien@hotmail.fr>
2022-11-16 21:00:48 +0100

    Ajout du dossier components dans le dossier views

---
+++ resources/views/components/movie-card.blade.php
@@ -0,0 +1,28 @@
+
+@props(['movie'])
+
+<div class="block-cards-movies">
+    <div class="flex-cards-movies">
+        <div class="movie-card">
+            <div class="movie-img">
+                <img src="{{asset('images/Avatar_2.jpg')}}" alt="Avatar 2">
+            </div>
+
+            <div class="card-body">
+                <h2 class="movie-title">
+                    <a href="/movie/{{$movie->id}}">{{$movie->title}}</a>
+                </h2>
+                <span class="features">
+                    <a href="#">{{$movie->year}}</a>
+                </span>
+                <span class="features">
+                    <a href="#">{{$movie->duration}}</a>
+                </span>
+                <span class="features">
+                    <a href="#">{{$movie->studio}}</a>
+                </span>
+                <p class="movie-intro">{{$movie->story}}</p>
+            </div>
+        </div>
+    </div>
+ </div>
