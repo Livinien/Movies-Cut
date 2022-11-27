034c39af2913b64822f6c2627bdccaa391216b3f
Flavien <livinien@hotmail.fr>
2022-11-27 19:50:30 +0100

    petites modifications

--- database/migrations/2022_11_10_155245_create_movies_table.php
+++ database/migrations/2022_11_10_155245_create_movies_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
             $table->string('poster')->nullable();
             $table->string('title');
             $table->string('year');
--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -1,40 +1,41 @@
 
 @props(['movie'])
 
+
 <div class="block-cards-movies">
     <div class="flex-cards-movies">
         <div class="movie-card">
             <div class="movie-img">
                 <img src="{{$movie->poster ? asset('storage/' . $movie->poster) : asset('/images/Ready_Player_One.jpg')}}" alt="Posters movies">
-            </div>
+            
 
-            <div class="card-body">
-                <h2 class="movie-title">
-                    <a href="/movie/{{$movie->id}}">{{$movie->title}}</a>
-                </h2>
+                <div class="card-body">
+                    <h2 class="movie-title">
+                        <a href="/movie/{{$movie->id}}">{{$movie->title}}</a>
+                    </h2>
 
-                <span class="features">
-                    <a href="#">{{$movie->year}}</a>
-                </span>
+                    <span class="features">
+                        <a href="#">{{$movie->year}}</a>
+                    </span>
 
-                <span class="features">
-                    <a href="#">{{$movie->duration}}</a>
-                </span>
+                    <span class="features">
+                        <a href="#">{{$movie->duration}}</a>
+                    </span>
 
-                <span class="features">
-                    <a href="#">{{$movie->studio}}</a>
-                </span>
+                    <span class="features">
+                        <a href="#">{{$movie->studio}}</a>
+                    </span>
 
-                <div class="readMore">
-                    <div class="content-story">
-                        <p>{{$movie->story}}</p>
+                    <div class="readMore">
+                        <div class="content-story">
+                            <p>{{$movie->story}}</p>
+                        </div>
+                        <a href="/movie/{{$movie->id}}" class="more-informations">More Informations</a>
                     </div>
-                    <a href="/movie/{{$movie->id}}" class="more-informations">More Informations</a>
-                </div>
 
 
                 </div>
             </div>
         </div>
     </div>
- </div>
+</div>
